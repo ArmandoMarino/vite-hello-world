4c79d6ab30d9ffa2f67e859d4e8e3849f210d9c4
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-01-19 16:00:05 +0100

    added img and method for path

---
+++ src/components/AppHero.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'AppHero',
+    data() {
+        return {
+            images: ['banner.jpg'],
+        }
+    },
+    methods: {
+        buildImagePath(image) {
+            const url = new URL(`.assets/img/${image}`, import.meta.url);
+            return url.href;
+        }
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style>
+
+</style>
